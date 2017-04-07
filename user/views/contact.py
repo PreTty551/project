4078ec43d259f999dec49fa29aa5aaac27eb5ae8
mengwei <wjxh607@gmail.com>
2017-04-07 13:24:34 +0800

    重构了views, 将views拆开分开写

---
+++ user/views/contact.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import json
+
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseBadRequest, Http404, \
+        HttpResponseServerError, HttpResponseNotFound
+
+from corelib.http import JsonResponse
+from corelib.decorators import login_required_404
+
+from user.consts import APPSTORE_MOBILE, ANDROID_MOBILE, SAY_MOBILE
+from user.models import User, UserContact, InviteFriend, Friend, ContactError
+
+
+def get_contacts(request):
+    contact_list = UserContact.get_all_contact(user_id=request.user.id)
+    return JsonResponse(contact_list)
+
+
+def get_contacts_in_app(request):
+    ignore_user_ids = Ignore.get_contacts_in_app(owner_id=request.user.id)
+    friend_ids = Friend.get_friend_ids(user_id=request.user.id)
+    all_mobile_list = list(UserContact.objects.filter(user_id=request.user.id).values_list("mobile", flat=True))
+    user_ids = list(User.objects.filter(mobile__in=all_mobile_list)
+                                .exclude(id__in=ignore_user_ids)
+                                .exclude(id__in=friend_ids)
+                                .exclude(id__in=invite_friends)
+                                .values_list("id", flat=True))
+    invite_friends = InviteFriend.get_friend_invites(user_id=request.user.id, user_ranges=user_ids)
+    user_ids = invite_friends.extend(user_ids[:10])
+
+    result = []
+    for user_id in user_ids:
+        user = User.get(id=user_id)
+        basic_info = user.basic_info()
+        # is_invited_user = Friend.is_invited_user(user_id=owner_id, friend_id=user_id)
+        # basic_info["is_invited_user"] = is_invited_user
+        result.append(basic_info)
+    # return result
+    return JsonResponse(result)
+
+
+def get_contacts_out_app(request):
+    contact_list = UserContact.get_contacts_out_app(owner_id=request.user.id)
+    return JsonResponse(contact_list)
+
+
+@login_required_404
+def add_user_contact(request):
+    contact = request.POST.get("contact")
+    contact_list = json.loads(contact)
+    is_success = UserContact.bulk_add(contact_list=contact_list, user_id=request.user.id)
+    if is_success:
+        user = User.get(request.user.id)
+        user.set_props_item("is_import_contact", 1)
+        return JsonResponse()
+    return HttpResponseServerError()
+
+
+@login_required_404
+def update_user_contact(request):
+    contact = request.POST.get("contact")
+    contact_list = json.loads(contact)
+    contact_list = UserContact.clean_contact(contact_list=contact_list)
+    my_all_contact = UserContact.get_all_contact(user_id=request.user.id)
+    new_contact_list = [o for o in contact_list if o not in my_all_contact]
+    for uc in new_contact_list:
+        obj = UserContact.objects.filter(user_id=request.user.id, mobile=uc["mobile"]).first()
+        if obj:
+            obj.name = uc["name"]
+            obj.save()
+        else:
+            UserContact.objects.create(name=uc["name"],
+                                       mobile=uc["mobile"],
+                                       user_id=request.user.id)
+    return JsonResponse()
---
+++ user/views/friend.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseBadRequest, Http404, \
+        HttpResponseServerError, HttpResponseNotFound
+
+from corelib.http import JsonResponse
+from corelib.decorators import login_required_404
+from corelib.leancloud import LeanCloud
+
+from user.models import User, UserContact, InviteFriend, Friend, ContactError
+from user.models import Ignore
+
+
+def invite_friend(request):
+    invited_id = request.POST.get("invited_id")
+    is_success = InviteFriend.add(user_id=request.user.id, invited_id=invited_id)
+    if is_success:
+        # message = "%s 邀请你加入好友" % request.user.nickname
+        # LeanCloud.async_push(receive_id=invited_id, message=message)
+        return JsonResponse()
+    return HttpResponseServerError()
+
+
+def agree_friend(request):
+    invited_id = request.POST.get("invited_id")
+    is_success = InviteFriend.agree(user_id=request.user.id, invited_id=invited_id)
+    if is_success:
+        # message = "%s 同意了你的好友请求" % request.user.nickname
+        # LeanCloud.async_push(receive_id=invited_id, message=message)
+        return JsonResponse()
+    return HttpResponseServerError()
+
+
+def ignore(request):
+    user_id = request.POST.get("user_id")
+    ignore_type = request.POST.get("ignore_type")
+    ignore = Ignore.add(owner_id=request.user.id, user_id=user_id, ignore_type=ignore_type)
+    if ignore:
+        return JsonResponse()
+    return HttpResponseServerError()
+
+
+def get_friends(request):
+    friend_ids = Friend.get_friend_ids(user_id=request.user.id)
+    friend_list = [User.get(user_id=friend_id).to_dict() for friend_id in friend_ids]
+    return JsonResponse(friend_list)
+
+
+def get_friend_invites(request):
+    pass
