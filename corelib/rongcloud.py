2f5b4f3bffd474293be74ac9b1b4149ae4a4fd12
mengwei <wjxh607@gmail.com>
2017-04-14 17:07:03 +0800

    rongcloud

---
+++ corelib/rongcloud.py
@@ -0,0 +1,16 @@
+from rongcloud import RongCloud as RC
+
+from django.conf import settings
+
+
+class RongCloud(object):
+
+    def __init__(self):
+        self.rongcloud = RC(settings.RONGCLOUD_APP_KEY, settings.RONGCLOUD_APP_SECRET)
+
+    def publish_message(self, user_id, to_user_id, content):
+        return self.rongcloud.Message.publishPrivate(
+                    fromUserId=user_id,
+                    toUserId=to_user_id,
+                    objectName='IDA:InstMsg',
+                    content=content)
