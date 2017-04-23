c25296ec3873f7068df4e49f145bcf2c09c4d5d8
mengwei <wjxh607@gmail.com>
2017-04-23 14:37:35 +0800

    修改上传头像的名字和保存方式

--- corelib/kingsoft/ks3.py
+++ corelib/kingsoft/ks3.py
@@ -1,3 +1,6 @@
+import time
+import requests
+
 from ks3.connection import Connection
 
 from django.conf import settings
@@ -9,12 +12,12 @@ class KS3(object):
         self.client = Connection(settings.KS3_KEY, settings.KS3_SECRET, host="ks3-cn-beijing.ksyun.com")
         self.bucket = self.client.get_bucket("ida-avatar")
 
-    def fetch(self, url, filename=None, headers={"Content-Type": "image/png"}):
+    def fetch(self, url, filename=None, headers={"Content-Type": "image/jpeg"}):
         content = requests.get(url).content
         obj = self.bucket.new_key(filename)
-        return obj.set_contents_from_string(content, headers=headers)
+        return obj.set_contents_from_string(content, headers=headers, policy="public-read")
 
     def fetch_avatar(self, url, user_id):
-        filename = "%s.png" % user_id
+        filename = "%s_%s.jpg" % (user_id, int(time.time() * 1000))
         self.fetch(url=url, filename=filename)
         return filename
