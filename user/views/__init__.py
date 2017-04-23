14191d1cc4b795464725cd2021638cdd23d07098
mengwei <wjxh607@gmail.com>
2017-04-23 21:28:15 +0800

    import bugfix

--- user/views/__init__.py
+++ user/views/__init__.py
@@ -1,4 +1,5 @@
-from .contact import get_contacts, get_contacts_in_app, get_contacts_out_app, add_user_contact, update_user_contact
+from .contact import get_contacts, get_contacts_in_app, get_contacts_out_app, add_user_contact, update_user_contact, \
+                     get_contact_list, common_contact
 from .friend import invite_friend, agree_friend, ignore, get_friends, get_friends_order_by_pinyin, update_user_memo, \
                     who_is_friends, delete_friend, update_invisible, update_push
 from .user import request_sms_code, request_voice_code, verify_sms_code, register, wx_user_login, wb_user_login, \
