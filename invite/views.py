7d87964a0fdbe815ae5959adacbc62209f278249
qingfeng <qingfenghello@gmail.com>
2017-04-25 00:18:42 +0800

    同步h5页的线上修改

--- invite/views.py
+++ invite/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-# from user.models import User
+
+
+from user.models import User
+from live.models import ChannelMember, Channel
 
 
 def wechat(request, user_id):
-    # user = User.get(id=user_id)
-    user = ""
-    return render(request, 'invite/index.html', {'user_id': user_id})
+    user = User.get(id=user_id)
+    channelmember = ChannelMember.objects.filter(user_id=user_id).first()
+    if channelmember:
+        channel = Channel.get_channel(channelmember.channel_id)
+        is_lock = channel.is_lock
+        channel_id = channel.channel_id
 
-def play(request, user_id):
-    # user = User.get(id=user_id)
-    user = ""
-    return render(request, 'invite/play.html', {'user_id': user_id})
+        if is_lock:
+            return render(request, 'invite/index1.html', {'user': user})
+
+        return render(request, 'invite/index.html', {'user': user, 'channel_id': channel_id})
+    else:
+        return render(request, 'invite/index1.html', {'user': user})
+
