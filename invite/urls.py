43116d4cbc7f3d6ef85058f3e1af7f1bde87c584
qingfeng <qingfenghello@gmail.com>
2017-04-24 22:46:42 +0800

    进入h5页播放页

--- invite/urls.py
+++ invite/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<user_id>\d+)/wechat/$', views.wechat, name='invite_wechat'),
+    url(r'^(?P<user_id>\d+)/play/$', views.play, name='play_wechat'),
 ]
--- invite/views.py
+++ invite/views.py
@@ -5,4 +5,9 @@ from django.shortcuts import render
 def wechat(request, user_id):
     # user = User.get(id=user_id)
     user = ""
-    return render(request, 'invite/index.html', {'user': user})
+    return render(request, 'invite/index.html', {'user_id': user_id})
+
+def play(request, user_id):
+    # user = User.get(id=user_id)
+    user = ""
+    return render(request, 'invite/play.html', {'user_id': user_id})
