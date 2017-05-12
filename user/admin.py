8f4926fbe5b569d8f4a018f4e74572d45f39be1d
mengwei <mengwei607@gmail.com>
2017-05-12 15:00:58 +0800

    禁用用户后台

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.models import BanUser
+
+
+@admin.register(BanUser)
+class BanUserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'get_nickname', 'desc')
+
+    def get_nickname(self, obj):
+        return User.get(obj.user_id).nickname
+    get_nickname.short_description = u"昵称"
