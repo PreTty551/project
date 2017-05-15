0676affd0915acfc2a7cb5899321e99fabd2ca2a
mengwei <mengwei607@gmail.com>
2017-05-15 16:02:54 +0800

    ida兼职人员统计

--- ida/management/commands/duty.py
+++ ida/management/commands/duty.py
@@ -10,6 +10,84 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        for gid in range(1, 4):
-            duty_ids = list(Duty.objects.filter(group=gid).values_list("user_id", flat=True))
-            duty_user(duty_ids, gid)
+        # for gid in range(1, 4):
+        #     duty_ids = list(Duty.objects.filter(group=gid).values_list("user_id", flat=True))
+        #     duty_user(duty_ids, gid)
+
+        DD = "2017-05-14 00:00:00"
+        aaa = """170418
+170419
+170440
+170416
+170409
+170425
+170430
+170417
+170415
+170427
+170407
+170408
+170442
+170414
+170447
+170510
+170568
+170613
+170589
+170747
+170756
+170439
+170785
+"""
+        user_ids = [user_id for user_id in aaa.split("\n") if user_id]
+        start = datetime.datetime.strptime(DD, '%Y-%m-%d %H:%M:%S')
+        end = start + datetime.timedelta(days=1)
+        duty_user(user_ids, 1, start, end)
+
+        bbb = """170504
+170325
+170388
+170480
+170375
+170352
+170343
+170396
+170522
+170326
+170337
+170395
+170509
+170539
+170448
+170398
+170378
+170648
+170504
+170342
+170674
+170646
+170356
+170810
+170716
+170764
+170388
+170805
+170760
+170741
+170739
+170691
+170457
+170615
+170809
+170748
+170725
+170714
+170740
+170664
+170824
+170900
+170901"""
+        user_ids = [user_id for user_id in bbb.split("\n") if user_id]
+        start = datetime.datetime.strptime(DD, '%Y-%m-%d %H:%M:%S')
+        end = start + datetime.timedelta(days=1)
+        duty_user(user_ids, 2, start, end)
--- ida/models.py
+++ ida/models.py
@@ -1,34 +1,39 @@
+import datetime
+
 from django.db import models
+from user.models import User, Friend, ChannelAddFriendLog
+from live.consts import ChannelType
 
 
 class Duty(models.Model):
     user_id = models.IntegerField(db_index=True)
-    name = models.CharField(max_length=50)
-    mobile = models.CharField(max_length=20)
-    group = models.SmallIntegerField()
+    name = models.CharField(max_length=50, default="")
+    mobile = models.CharField(max_length=20, default="")
+    group = models.SmallIntegerField(default=1)
 
 
-def duty_user(user_ids, group):
+def duty_user(user_ids, group, start_date, end_date):
     public_party_ids = []
     friend_party_ids = []
 
-    table_title = "日期 值班人ID 昵称 朋友总数 好友party 总数 公开party 总数\n"
-    f = open("/home/mengwei/duty_g%s.csv" % group, "w+")
+    table_title = "值班人ID,昵称,朋友总数,好友party,总数,公开party,总数\n"
+    d = start_date.strftime('%Y-%m-%d')
+    f = open("/home/mengwei/duty/duty_g%s_%s.csv" % (group, d), "w+")
     f.writelines(table_title)
     for user_id in user_ids:
         if not user_id:
             continue
 
         user = User.get(user_id)
-        friend_ids = list(Friend.objects.filter(user_id=user_id).values_list("friend_id", flat=True))
-        logs = ChannelAddFriendLog.objects.filter(user_id=user_id).values_list("friend_id", "channel_type").distinct()
+        friend_ids = list(Friend.objects.filter(user_id=user_id, date__gte=start_date, date__lt=end_date).values_list("friend_id", flat=True))
+        logs = ChannelAddFriendLog.objects.filter(user_id=user_id, date__gte=start_date, date__lt=end_date).values_list("friend_id", "channel_type").distinct()
         for friend_id, channel_type in logs:
             if channel_type == ChannelType.public.value:
                 public_party_ids.append(friend_id)
             else:
                 friend_party_ids.append(friend_id)
 
-        logs = ChannelAddFriendLog.objects.filter(friend_id=user_id).values_list("user_id", "channel_type").distinct()
+        logs = ChannelAddFriendLog.objects.filter(friend_id=user_id, date__gte=start_date, date__lte=end_date).values_list("user_id", "channel_type").distinct()
         for user_id, channel_type in logs:
             if channel_type == ChannelType.public.value:
                 if user_id not in public_party_ids:
@@ -42,8 +47,7 @@ def duty_user(user_ids, group):
         public_party_ids = [str(uid) for uid in public_party_ids]
         friend_party_ids = [str(uid) for uid in friend_party_ids]
 
-        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        data = [now, str(user_id), user.nickname, str(len(friend_ids)),
+        data = [str(user_id), user.nickname, str(len(friend_ids)),
                 " ".join(friend_party_ids), str(len(friend_party_ids)),
                 " ".join(public_party_ids), str(len(public_party_ids))]
         f.write(",".join(data) + "\n")
