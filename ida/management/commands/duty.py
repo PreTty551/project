cee7931a44b961da05a7ae1d85d93b0ddf6ff4a3
mengwei <mengwei607@gmail.com>
2017-05-24 21:31:55 +0800

    修改统计值班加好友的脚本

--- ida/management/commands/duty.py
+++ ida/management/commands/duty.py
@@ -15,6 +15,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         duty_ids = list(Duty.objects.filter(group="黑白校园").values_list("user_id", flat=True))
         today_str = datetime.datetime.strftime(timezone.now(), "%Y-%m-%d")
-        end = datetime.datetime.strptime(today_str, '%Y-%m-%d')
-        start = start + datetime.timedelta(days=-1)
-        duty_user_add_friend(duty_ids, 1, start, end)
+        for i in list(range(15, 22)):
+            start = datetime.datetime.strptime("2017-05-%s" % i, '%Y-%m-%d')
+            end = start + datetime.timedelta(days=1)
+            duty_user_add_friend(duty_ids, 1, start, end)
