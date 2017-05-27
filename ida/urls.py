52c25877eb0d8ae83d8feed19444c4f16e831235
mengwei <mengwei607@gmail.com>
2017-05-27 17:25:57 +0800

    简易踢人后台

--- ida/urls.py
+++ ida/urls.py
@@ -6,5 +6,5 @@ from ida import views
 
 urlpatterns = [
     url(r'^duty/live_time/$', views.duty_live_party_time, name='duty_live_party_time'),
-
+    url(r'^ida/tick/$', views.tick, name='tick'),
 ]
--- ida/views.py
+++ ida/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from ida.models import duty_party_time
+from user.models import fuck_you
 
 
 def duty_live_party_time(request):
@@ -11,3 +12,11 @@ def duty_live_party_time(request):
     user_ids = user_ids.split("\n")
     results = duty_party_time(user_ids=user_ids, start_date=start_date, days=days)
     return render(request, 'ida/live_party_time.html', {'results': results})
+
+
+def tick(request):
+    user_id = request.POST.get("user_id", "")
+
+    if request.method == "POST":
+        fuck_you(user_id)
+    return render(request, 'ida/tick.html')
